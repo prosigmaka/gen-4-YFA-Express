1c23eee98cc1645ea34ffdd61d50ae9b2e11a03a
yyn <jr.yayan@gmail.com>
2021-04-23 15:39:57 +0700

    admin and user

---
+++ src/main/java/com/kel3/yfaexpress/model/dto/UserDto.java
@@ -0,0 +1,13 @@
+package com.kel3.yfaexpress.model.dto;
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private Integer idUser;
+    private String UserName;
+    private String emailUser;
+    private String fullName;
+    private Number telepon;
+    private String email;
+}
+
