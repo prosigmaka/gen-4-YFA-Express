84beed8346ec47818fa003bf96c150d316ef4194
adisugiarto <adisugiarto270@gmail.com>
2021-04-26 18:52:45 +0700

    berat barang

--- src/main/java/com/kel3/yfaexpress/controller/restapi/ApiBeratBarang.java
+++ src/main/java/com/kel3/yfaexpress/controller/restapi/ApiBeratBarang.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/berat")
+@RequestMapping("/api/berat-barang")
 public class ApiBeratBarang {
     @Autowired
     private BeratBarangRepository beratBarangRepository;
