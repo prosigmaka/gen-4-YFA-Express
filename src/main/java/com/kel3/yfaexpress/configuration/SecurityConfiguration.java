36675078bd3ef1ff2be27738888d19c92a0c1e34
yyn <jr.yayan@gmail.com>
2021-04-26 15:34:09 +0700

    SecurityConfiguration

--- src/main/java/com/kel3/yfaexpress/configuration/SecurityConfiguration.java
+++ src/main/java/com/kel3/yfaexpress/configuration/SecurityConfiguration.java
@@ -39,12 +39,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers(
-				 "/registration**",
-	                "/js/**",
-	                "/css/**",
-	                "/img/**").permitAll()
-		.anyRequest().authenticated()
+		http.authorizeRequests()
+		.antMatchers("/registration**", "/js/**", "/css/**", "/img/**")
+		.permitAll()
+		.anyRequest()
+		.authenticated()
 		.and()
 		.formLogin()
 		.loginPage("/login")
