a3892232cbc7c9f87e19cfe60967c0c27849256a
yyn <jr.yayan@gmail.com>
2021-04-29 11:17:43 +0700

    login+security

--- src/main/java/com/kel3/yfaexpress/configuration/SecurityConfiguration.java
+++ src/main/java/com/kel3/yfaexpress/configuration/SecurityConfiguration.java
@@ -39,11 +39,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers(
-				 "/registration**",
-	                "/js/**",
-	                "/css/**",
-	                "/img/**").permitAll()
+		http.authorizeRequests().antMatchers("/registration**", "/js/**", "/css/**", "/img/**").permitAll()
 		.anyRequest().authenticated()
 		.and()
 		.formLogin()
--- src/main/java/com/kel3/yfaexpress/model/entity/User.java
+++ src/main/java/com/kel3/yfaexpress/model/entity/User.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.util.Collection;
 
 @Entity
-@Table(name =  "user")
+@Table(name = "user")
 public class User {
 
 	@Id
